6a3519692725fd3d17fe110d7c25797d57cbd953
Bryan Lima <bryanosoriolima@gmail.com>
2026-01-05 17:25:07 -0300

    [added] new modules archives

---
+++ modules/loadModule.js
@@ -0,0 +1,19 @@
+export default async function LoadModule(item) {
+    try {
+        const res = await fetch(`./modules/${item}.html`);
+        if(!res.ok) throw new Error("Error: module is not loading");
+
+        return await res.text();
+    } 
+    catch(err) {
+        console.warn("Page error dont working properly, trying redirecting to 'error.html' page");
+
+        try {
+            const resError = await fetch(`./modules/error.html`);
+            return await resError.text();
+        } 
+        catch {
+            return "<h3> ERRO CRITICO! Modulo não carregará. Entre em contato com os desenvolvedores</h3>";
+        }
+    }
+} 
