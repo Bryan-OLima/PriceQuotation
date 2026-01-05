cfc0f68d1d86cc3aec1650c62135a113990cb3e0
Bryan Lima <bryanosoriolima@gmail.com>
2026-01-05 16:38:18 -0300

    [NEW] form receiving data

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import LocalStorageService from './services/localStorageService.js';
+import PriceQuotation from './dtos/priceQuotation.js';
 
-const button = document.querySelector(".button-send");
-
-button.addEventListener("click", () => {
-    console.log("clicked")
-});
+const form = document.querySelector("form");
 
 const data = LocalStorageService();
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const formData = new FormData(form);
+    
+    const filament = parseFloat(formData.get("filament"));
+    const hours = parseFloat(formData.get("hours"));
+    const workedHours = parseFloat(formData.get("workedHours"));
+    const timeStamp = Date.now();
+    const isNoProfit = formData.get("isNoProfit") === "on";
+
+    const priceQuotation = new PriceQuotation(filament, hours, workedHours, isNoProfit, timeStamp);
+
+    console.log(isNoProfit);
+    console.log(Object.fromEntries(formData), ",", priceQuotation)
+});
 
 console.log('aaaa',data.getAll());
